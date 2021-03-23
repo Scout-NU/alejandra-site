5b693427578f41ce78d47767bddf48f5c5aa02bf
jzhzhang66 <jzhzhang66@gmail.com>
2021-03-23 18:44:31 -0400

    Added padding to box link component

--- components/BoxLink.js
+++ components/BoxLink.js
@@ -3,19 +3,28 @@ import styled from 'styled-components';
 
 const BoxLinkTitle = styled.h2`
     font-size: 24px;
+    padding: 30px 40px 0 40px;
+    position: absolute;
 `;
+
+// top right bottom left
 
 const BoxLinkDescription = styled.p`
     font-size: 16px;
+    padding: 0 40px 40px 30px;
+    position: absolute;
 `;
 
 const Box = styled.div`
     margin: auto;
+    display: flex;
+    position: relative;
     overflow: hidden;
-    width: 80%;
+    max-width: 500px;
     background-color: #DEB887;
     border-radius: 19px;
 `;
+
 export default function BoxLink({ boxLinkData }) {
     const {
         box_link_title: boxLinkTitle,
@@ -27,7 +36,6 @@ export default function BoxLink({ boxLinkData }) {
         <Box>
             <BoxLinkTitle>{getString(boxLinkTitle)}</BoxLinkTitle>
             <BoxLinkDescription>{getString(boxLinkDescription)}</BoxLinkDescription>
-            {boxLinkLink}
         </Box>
     </>
   );
