57608ad650c5671e9b3d0cbbef68065b0f2c03d8
jzhzhang66 <jzhzhang66@gmail.com>
2021-03-25 09:20:43 -0400

    BoxLink responsiveness in progress

--- components/BoxLink.js
+++ components/BoxLink.js
@@ -1,17 +1,24 @@
 import { getString } from "../lib/richText";
 import styled from 'styled-components';
 import BoxLinkArrow from "../icons";
+import { min, max } from "../lib/responsive";
 
 const BoxLinkTitle = styled.h2`
     font-size: 24px;
     padding: 1em 2em 0 2em;
+    @media ${max.tabletLg} {
+        padding: 0 1em 0 1em;
+      }
 
 `;
 
 const BoxLinkDescription = styled.p`
     font-size: 16px;
-    padding: 0 2em 1.8em 3em;
+    padding: 0 2em 1.5em 3em;
     margin-top: .5em;
+    @media ${max.tabletLg} {
+        padding: 0 1em 0 1.6em;
+      }
 `;
 
 const Box = styled.div`
@@ -20,10 +27,16 @@ const Box = styled.div`
     max-width: 25em;
     background-color: #C4C4C4;
     border-radius: 19px;
+    @media ${max.tabletLg} {
+        width: 40%;
+      }
 `;
 
 const Arrow = styled.div`
     padding: 1.5em 0 0 17em;
+    @media ${max.tabletLg} {
+        padding: 1em 0 0 0;
+      }
 `;
 
 export default function BoxLink({ boxLinkData }) {
