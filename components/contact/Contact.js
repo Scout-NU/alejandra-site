2a2336b789bf270810bdf8bb277f7b2532d7a821
jzhzhang66 <jzhzhang66@gmail.com>
2021-04-06 00:04:42 -0400

    Removed contact page images

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -10,8 +10,6 @@ export default function ContactPage({ contactPageData }) {
     contact_page_title_regular: contactPageTitleRegular,
     contact_page_title_italic: contactPageTitleItalic,
     contact_page_description: contactPageDescription,
-    contact_image_top: contactImageTop,
-    contact_image_bottom: contactImageBottom,
     contact_confirmation_title_regular_1,
     contact_confirmation_title_italic_2,
     contact_confirmation_title_regular_3,
@@ -35,8 +33,6 @@ export default function ContactPage({ contactPageData }) {
       ></H1Styled>
       <SvgStyled />
       <P>{getString(contactPageDescription)}</P>
-      <img src={contactImageTop.url} alt={contactImageTop.alt} />
-      <img src={contactImageBottom.url} alt={contactImageBottom.alt} />
       <ContactConfirmation contactConfirmationData={contactConfirmationData} />
     </>
   );
