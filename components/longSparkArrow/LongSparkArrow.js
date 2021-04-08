0b6cb7ea5e30e05ccae7e030be16ccab1a874173
jzhzhang66 <jzhzhang66@gmail.com>
2021-04-08 16:58:29 -0400

    Spark hover state working on desktop

--- components/longSparkArrow/LongSparkArrow.js
+++ components/longSparkArrow/LongSparkArrow.js
@@ -1,18 +1,30 @@
-import { getString } from '../../lib/richText';
 import {
     UnfilledSparkArrow,
     ArrowText,
     ArrowContainer,
+    ArrowContainerHover,
+    FilledSparkArrow,
+    ArrowTextHover
 } from './LongSparkArrow.styles';
 import { icons } from '../../style/icons';
+import React, { useState } from 'react';
 
-export default function LongSparkArrow({ arrowText }) {
+export default function LongSparkArrow({ arrowText, onClick }) {
+    const [hover, setHover] = useState(false);
     return (
-        <>
-            <ArrowContainer>
-                <UnfilledSparkArrow src={icons.LONG_UNFILLED_SPARK_ARROW} />
-                <ArrowText>{arrowText}</ArrowText>
-            </ArrowContainer>
-        </>
+        <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} onClick={() => onClick}>
+          
+                {!hover &&
+                    <ArrowContainer >
+                        <UnfilledSparkArrow src={icons.LONG_UNFILLED_SPARK_ARROW} />
+                        <ArrowText>{arrowText}</ArrowText>
+                    </ArrowContainer>}
+                {hover &&
+                    <ArrowContainerHover >
+                        <FilledSparkArrow src={icons.LONG_FILLED_SPARK_ARROW} />
+                        <ArrowTextHover>{arrowText}</ArrowTextHover>
+                    </ArrowContainerHover>}
+
+        </div>
     );
 }
--- components/longSparkArrow/LongSparkArrow.styles.js
+++ components/longSparkArrow/LongSparkArrow.styles.js
@@ -1,6 +1,7 @@
 import { P } from '../../style/typography';
 import styled from 'styled-components';
 import { colors } from '../../style/colors';
+import { fontWeights } from '../../style/typography';
 import { min } from '../../lib/responsive';
 
 const UnfilledSparkArrow = styled.img` 
@@ -24,6 +25,48 @@ const UnfilledSparkArrow = styled.img`
 }
 `;
 
+const FilledSparkArrow = styled.img` 
+  position: absolute;
+  transform: rotate(90deg);
+  z-index: 1;
+  @media ${min.mobile} {
+    top: -.2em;
+    right: 11em;
+    height: 28em;
+  }
+  @media ${min.tablet} {
+    top: -.2em;
+    right: 8em;
+    height: 20em;
+  }
+@media ${min.desktop} {
+  top: 2em;
+  right: 8em;
+  height: 18em;
+}
+`;
+
+const ArrowTextHover = styled(P)`
+position: absolute;
+z-index: 2;
+right: 0;
+text-align: right;
+color: ${colors.BROWN};
+font-weight: ${fontWeights.bold};
+@media ${min.mobile} {
+  padding-top: 4.5em;
+  width: 20em;
+}
+@media ${min.tablet} {
+  padding-top: 4.2em;
+  width: 20em;
+}
+@media ${min.desktop} {
+padding-top: 9.3em;
+width: 20em;
+}
+`;
+
 const ArrowText = styled(P)` 
   position: absolute;
   z-index: 2;
@@ -42,8 +85,20 @@ const ArrowText = styled(P)`
   padding-top: 7.8em;
   width: 20em;
 }
+`;
 
-
+const ArrowContainerHover = styled.div`
+  position: absolute;
+  transform: rotate(270deg);
+  @media ${min.mobile} {
+    left: -15em;
+    }
+  @media ${min.tablet} {
+    left: -10em;
+    }
+  @media ${min.desktop} {
+  left: -9em;
+  }
 `;
 
 const ArrowContainer = styled.div`
@@ -64,4 +119,7 @@ export {
     UnfilledSparkArrow,
     ArrowText,
     ArrowContainer,
+    ArrowTextHover,
+    FilledSparkArrow,
+    ArrowContainerHover
 };
