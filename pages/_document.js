6a3ec4926fa16650ba16c9e7026a8351e68d0969
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-05-12 23:57:16 -0700

    temp

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import { colors } from '../style/colors';
+import { apiEndpoint } from './../sm.json';
+const prismicRepoName = /([\dA-Za-z-]+)?(\.cdn)?\.prismic\.io/.exec(
+  apiEndpoint,
+)[1]; //Regex to get repo ID
 
 export default class MyDocument extends Document {
   render() {
@@ -8,6 +12,11 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="stylesheet" href="https://use.typekit.net/jds8ebt.css" />
+          <script
+            async
+            defer
+            src={`//static.cdn.prismic.io/prismic.js?repo=${prismicRepoName}&new=true`}
+          />
         </Head>
         <body style={{ backgroundColor: colors.CREME }}>
           <Main />
